327ba1c4e8966a92b0cd6d4fb3c9d3dfe4cfe89f
joshuef <joshuef@gmail.com>
2010-03-19 12:40:50 +0000

    Removed default index

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe PagesController do
   integrate_views
+  before(:each) do
+    
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+    
+  end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -12,9 +17,23 @@ describe PagesController do
     it "should have the right title" do
       get 'home'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Home")
+                              @base_title + "Home")
     end
   end
+
+
+  describe "GET 'help'" do
+    it "should be successful" do
+    get 'help'
+    response.should be_success
+    end
+  
+  it "should have the right title" do
+    get 'help'
+    response.should have_tag("title",
+                            @base_title + "Help")
+  end
+end
 
   describe "GET 'contact'" do
     it "should be successful" do
@@ -25,7 +44,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | Contact")
+                               @base_title + "Contact")
     end
   end
 
@@ -38,7 +57,7 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_tag("title",
-                               "Ruby on Rails Tutorial Sample App | About")
+                               @base_title + "About")
     end
   end
 end
